4338dcaeb7d91ba21b9acd2b338a843a79f3caa0
Yang Kefan <569283998@qq.com>
2017-06-04 00:33:08 +0800

    implement BPlusTree search

--- src/IndexManager/BPlusTree.h
+++ src/IndexManager/BPlusTree.h
@@ -3,17 +3,17 @@
 #include "../BufferManager/Block.h"
 #include "../BufferManager/BufferManager.h"
 
-template<class T,int order>
-class BPlusNode:public Block{
+template<class T, int order>
+class BPlusNode :public Block {
 public:
-	bool & isLeaf(){
+	bool & isLeaf() {
 		return *(bool*)(&block_data[BLOCK_HEAD_SIZE]);
 	}
-	uint32_t & parent(){
+	uint32_t & parent() {
 		return *(uint32_t*)(&block_data[BLOCK_HEAD_SIZE + 1]);
 	}
 	//unsigned int & order() {
-		//return *(unsigned int*)(&block_data[BLOCK_HEAD_SIZE + 5]);
+	//return *(unsigned int*)(&block_data[BLOCK_HEAD_SIZE + 5]);
 	//}
 	int & dataCnt() {
 		return *(int*)(&block_data[BLOCK_HEAD_SIZE + 9]);
@@ -22,18 +22,27 @@ public:
 		return (T*)(&block_data[BLOCK_HEAD_SIZE + 13]);
 	}
 	uint32_t *ptrs() {
-		return (uint32_t*)(&block_data[BLOCK_HEAD_SIZE + 13 + order*sizeof(T)]);//not order-1: one more position for split easily
+		return (uint32_t*)(&block_data[BLOCK_HEAD_SIZE + 13 + order * sizeof(T)]);//not order-1: one more position for split easily
 	}
 };
 
-template<class T,int order>
+//search result class
+//contains the block and the index of the result
+template<class T, int order>
+class BPlusTreeSearchResult {
+public:
+	int index;
+	BPlusNode<T, order>* node;
+};
+
+template<class T, int order>
 class BPlusTree {
 public:
-	BPlusTree(BPlusNode<T, order> *root = nullptr,unsigned int keyLen=25) :root(root),keyLen(keyLen) {
+	BPlusTree(BPlusNode<T, order> *root = nullptr, unsigned int keyLen = 25) :root(root), keyLen(keyLen) {
 		bufferManager = &BufferManager::Instance();
 	};
-	virtual ~BPlusTree() {};
-	//insert a record into the b+tree
+	virtual ~BPlusTree() {}
+	//insert a entry into the b+tree
 	void insert(T key, uint32_t addr) {
 		//std::cout << key;
 		BPlusNode<T, order>* theNode;
@@ -51,9 +60,30 @@ public:
 		//normal case
 		theNode = root;
 		while (!theNode->isLeaf()) {
-			theNode = static_cast<BPlusNode<T, order>*>(bufferManager->GetBlock(theNode->ptrs()[findSmallerInBlock(key,theNode) + 1]));
+			theNode = static_cast<BPlusNode<T, order>*>(bufferManager->GetBlock(theNode->ptrs()[findLargerInBlock(key, theNode)]));
 		}
 		insertInBlock(key, addr, theNode);
+	}
+	//search according to the key
+	//return a pointer to BPlusTreeSearchResult object
+	//the result if the first matched key in the linked list
+	//or the smallest larger one
+	BPlusTreeSearchResult<T, order>* search(T key) {
+		int index;
+		BPlusNode<T, order>* theNode = root;
+		//retunr null pointer if the tree is empty
+		if (!root) {
+			return nullptr;
+		}
+		while (!theNode->isLeaf()) {
+			index = findFirstInBlock(key, theNode);
+			theNode = static_cast<BPlusNode<T, order>*>(bufferManager->GetBlock(theNode->ptrs()[index]));
+		}
+		index = findFirstInBlock(key, theNode);
+		BPlusTreeSearchResult<T, order>* result = new BPlusTreeSearchResult<T, order>();
+		result->index = index;
+		result->node = theNode;
+		return result;
 	}
 	//print out all leaf nodes
 	void printAll() {
@@ -75,7 +105,7 @@ public:
 		}
 		std::cout << std::endl;
 	}
-	//remove a record from the b+tree
+	//remove a entry from the b+tree
 	void remove(T key, uint32_t addr);
 	//remove the whole b+tree
 	void removeAll();
@@ -83,10 +113,10 @@ protected:
 	BPlusNode<T, order>* root;
 	unsigned int keyLen;
 	BufferManager* bufferManager;
-	// insert a new record
+	// insert a new entry
 	void insertInBlock(T key, uint32_t addr, BPlusNode<T, order>* theNode) {
 		//std::cout << '<'<<theNode->dataCnt() << '>';
-		int idx = this->findSmallerInBlock(key,theNode) + 1;
+		int idx = this->findLargerInBlock(key, theNode);
 		if (theNode->isLeaf()) {
 			for (int i = theNode->dataCnt() - 1;i >= idx;i--) {
 				theNode->data()[i + 1] = theNode->data()[i];
@@ -109,9 +139,8 @@ protected:
 			split(theNode);
 		}
 	}
-	//find the largest smaller data in the block
-	int findSmallerInBlock(T key, BPlusNode<T, order>* theNode)
-	{
+	//find the smallest larger entry in the block
+	int findLargerInBlock(T key, BPlusNode<T, order>* theNode) {
 		int low = 0, mid, high = theNode->dataCnt() - 1;
 		// binary search
 		while (low <= high)
@@ -123,10 +152,15 @@ protected:
 			else return mid;
 		}
 
-		// here return high if key is not found
-		// since we want the largest one that is smaller than key
-		// according to B+ Tree property
-		return high;
+		// return low if key is not found
+		return low;
+	}
+	//find the first matched key in the block
+	int findFirstInBlock(T key, BPlusNode<T, order>* theNode) {
+		int index = findLargerInBlock(key, theNode);
+		//trace back the first matched key
+		while (index > 0 && theNode->data()[index - 1] == key) index--;
+		return index;
 	}
 	//find the data in the block
 	int findInBlock(T key, BPlusNode<T, order>* theNode) {
@@ -166,21 +200,22 @@ protected:
 			newNode->dataCnt() = order / 2;
 			theNode->dataCnt() = order - order / 2;
 			//maintain linked list
-			newNode->ptrs()[order] = theNode -> ptrs()[order];
+			newNode->ptrs()[order] = theNode->ptrs()[order];
 			theNode->ptrs()[order] = newNode->BlockIndex();
-			//insert new record to parent
+			//insert new entry to parent
 			insertInBlock(newNode->data()[0], newNode->BlockIndex(), static_cast<BPlusNode<T, order>*>(bufferManager->GetBlock(newNode->parent())));
 		}
 		//non-leaf node
 		else {
-			memcpy(&(newNode->data()[0]), &(theNode->data()[order / 2 + 1]), (order - order / 2 - 1 )* sizeof(T));
+			memcpy(&(newNode->data()[0]), &(theNode->data()[order / 2 + 1]), (order - order / 2 - 1) * sizeof(T));
 			memcpy(&(newNode->ptrs()[0]), &(theNode->ptrs()[order / 2 + 1]), (order - order / 2) * sizeof(T));
 			newNode->isLeaf() = false;
 			newNode->parent() = theNode->parent();
 			newNode->dataCnt() = order - order / 2 - 1;
 			theNode->dataCnt() = order / 2;
-			//insert new record to parent
+			//insert new entry to parent
 			insertInBlock(theNode->data()[order / 2], newNode->BlockIndex(), static_cast<BPlusNode<T, order>*>(bufferManager->GetBlock(newNode->parent())));
 		}
 	}
 };
+
