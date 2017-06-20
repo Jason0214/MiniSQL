b1b3abf106a92aafc38bd975525b4b53c5b1e384
yzq <3150104172@zju.edu.cn>
2017-06-20 21:29:38 +0800

    api frame complete

--- src/API/MiniAPI.h
+++
@@ -1,9 +0,0 @@
-// The API module is implemented using low level modules including 
-// IndexManager,RecordManager,and CatalogManager. It can be used by
-// high level modules like Interpretor to carry out basic SQL operations
-#pragma once
-#include <string>
-#include <iostream>
-
-//select data from table, put new table in result
-void select(std::string table, std::string* operands, std::string* operators, int cnt, std::string result);
