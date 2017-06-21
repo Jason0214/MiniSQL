5f705e9ef3e56e37db81422dd016378acf60401e
Jason <793928798@qq.com>
2017-06-21 22:59:39 +0800

    update interface

---
+++ src/CatelogManager/Catelog.h
@@ -0,0 +1,73 @@
+#ifndef _CATELOG_H_
+#define _CATELOG_H_
+#include "../CONSTANT.h"
+#include <string>
+
+class TableMeta{
+public:
+	TableMeta(const std::string & table_name):table_name(table_name),attr_name_list(NULL),attr_type_list(NULL),
+					attr_num(0),table_addr(0),primay_key_index(-1),primary_index_addr(0){}
+	~TableMeta(){
+		delete [] this->attr_name_list;
+		delete [] this->attr_type_list;
+	}
+	const std::string table_name;
+	uint32_t table_addr;
+	int attr_num;
+	std::string* attr_name_list;
+	DBenum* attr_type_list;
+	int primay_key_index;
+	uint32_t primary_index_addr; // address of primary index
+	std::string & GetAttrName(int index){return this->attr_name_list[index];}
+	DBenum & GetAttrType(int index){return this->attr_type_list[index];}
+};
+
+class Catelog{
+public:
+	static Catelog & Instance(){
+		static Catelog theCatelog;
+		return theCatelog;
+	}
+	~Catelog(){}
+
+	void CreateDatabase(const std::string & db_name);
+	void UseDatabase(const std::string & db_name);
+<<<<<<< HEAD
+	void CreateTable(const std::string & table_name, std::string* attr_name_list, DBenum* attr_type_list, int attr_num, int key_index);
+	void GetTableAttr(TableMeta & table_meta);
+	void CreateIndex(const std::string & table_name, int8_t secondary_key_index);
+	uint32_t Catelog::GetIndex(const std::string & table_name, int8_t secondary_key_index);
+=======
+	void CreateTable(std::string & table_name, std::string* attr_name_list, DBenum* attr_type_list, unsigned int attr_num, int key_index);
+	void GetTableAttr(std::string & table_name, uint32_t & table_addr, uint32_t & index_addr,
+		std::string* attr_name_list, DBenum* attr_type_list, unsigned int & attr_num, unsigned int & key_index);
+//TODO
+	void CreateUser(const std::string user_name, const std::string passwd);
+
+	bool AuthenUser();
+	void SetPrivilege();
+
+	int TupleCount();
+	int TupleLen();
+	int BlockCount();
+	int BlockFactor(); // the number of distinct values that appear in the relation r for attribute A
+	
+>>>>>>> ba1a8288c001599b17f3fb266b24c10bd110e792
+private:
+	Catelog();
+	Catelog(const Catelog&);
+	Catelog & operator=(const Catelog&);
+
+	uint32_t database_block_addr;
+	uint32_t user_block_addr;
+
+	uint32_t table_data_addr;
+	uint32_t table_index_addr;
+
+	uint32_t index_data_addr;
+	uint32_t index_index_addr;
+
+	bool database_selected;
+};
+
+#endif
