088cc620624d3a6240033b354655d7098068b47b
yzq <3150104172@zju.edu.cn>
2017-06-30 18:00:51 +0800

    fix bugs

--- src/API/APIMain.cpp
+++ src/API/APIMain.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 static string Command;
 
-int main_api()
+int main()
 {
 	try
 	{
@@ -16,7 +16,7 @@ int main_api()
 		{
 			Command = GetString();
 
-			if (Command == "quit") return 0;
+			if (Command == "quit") goto _QUIT;
 			else if (Command == "begin_query") AcceptQuery();
 			else if (Command == "begin_insert") AcceptInsert();
 			else if (Command == "begin_update") AcceptUpdate();
